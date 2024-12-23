d99867601e640fed74e9d7460ce2706a92d6e0ac
priscila-yone-fujii <guingitsune78@gmail.com>
2024-12-22 22:14:31 -0300

    completo
    todas as operações estão funcionando e a estilização está boa.

--- simple/gerador.js
+++ simple/gerador.js
@@ -1,63 +1,68 @@
-function soma(a, b){ return a+b; }
-function sub(a, b){return a-b; }
-function multi(a, b){ return a*b; }
-function divi(a, b){ return a/b; }
-function mod(a, b){ return (a%b==0); }
-const arrOPs = ['+', '-', 'x', '/', 'mod'];
+/* ============================================*/
+function soma(x, y){ return x + y; }
+function subi(x, y){ return x - y; }
+function mult(x, y){ return x * y; }
+function divi(x, y){ return x / y; }
+function resto(x, y){
+    var str = '';
+    if(x%2==0){
+        str = `2`;
+    }else if(x%3==0){
+        str = `3`;
+    }
+    if(y%2==0){
+        str += `\t2`;
+    }else if(y%3==0){
+        str += `\t3`;
+    }
+    return str;
+}   
 
-function preencheTabela(tab, funclist){
-    var linhas = tab.querySelectorAll('.linha');
-    linhas.forEach((item, indice)=>{
+const btn = document.querySelector('#btn');
+const div = document.getElementById('container');
+var result = [];
+
+function gerarTabela(arr){
+    var table = document.createElement('table');
+    arr.forEach((elt, ind) => {
+        var tr = document.createElement('tr');
         var td = document.createElement('td');
-        td.className = 'coluna';
-        td.innerText = funclist[indice];
-        item.appendChild(td);
-        tab.appendChild(item);
+        td.innerText = `${elt.opr}`;
+        td.classList = `coluna`;
+        tr.className = `linha linha-${ind+1}`;
+        tr.appendChild(td);
+        table.appendChild(tr);
     });
+    return table;
+}
+function preecheTabela(tab, result){
+    var rows = tab.querySelectorAll('tr');
+    rows.forEach((row, ind)=> {
+        var td = document.createElement('td');
+        td.innerText = `${result[ind]}`;
+        td.className = `coluna col-result`;
+        row.appendChild(td);
+        tab.appendChild(row);
+    });
+    
     return tab;
 }
-
-function criaTabela(lista, table, x, y){
-    var div = document.body.querySelector('#container');
-    table.className = 'tabela'; 
-    lista.forEach((element, indice)=>{
-        var tr = document.createElement('tr');
-        tr.className = `linha linha-${indice}`;
-        var td = document.createElement('td');
-        td.className = `coluna coluna-${indice}`;  
-        td.innerText = `${x} ${element} ${y}`;
-        tr.appendChild(td);// problema está aqui
-        table.appendChild(tr);
-        div.appendChild(table);
-        
-    });
-    return div;
-}
-function estilizacao(tabela){
-    tabela.style.marginLeft = "100px";
-    tabela.style.justifyContent = "center";
-    tabela.style.alignItems = "center";
-    tabela.style = "border-top: 1px solid black;";
-    var linhas = tabela.querySelectorAll('.linha');
-    linhas.forEach((valor)=>{
-       valor.style.backgroundColor = "orange";
-       valor.style.fontStyle = "20px";
-    });
-}
-const btn = document.querySelector('.btn');
-
-
-// adicionando uma Classe: tagName.className = 'class Name';
-
-btn.addEventListener('click', function(){
-    const a = document.getElementById('um').value;
-    const b = document.getElementById('dois').value;
-    x = Number(a);
-    y = Number(b);
-    const functionList = [soma(x,y), sub(x,y), multi(x,y), mod(x,y)];
-    var table = document.createElement('table'); //Function create table? and others....
-    var novaTabela = criaTabela(arrOPs, table, x, y); 
-    var tabelacompleta = preencheTabela(novaTabela, functionList);
-    var tabelaEstilo = estilizacao(tabelacompleta);
-    
+/* ==================================================================*/
+btn.addEventListener('click', ()=>{
+    const a = Number(document.getElementById('um').value);
+    const b = Number(document.getElementById('dois').value);
+    const arr = [
+        {opr:'multiplicação', func: mult(a, b)},
+        {opr:'subitração', func: subi(a, b)},
+        {opr:'somatória', func: soma(a, b)},
+        {opr:'divisão', func: divi(a, b)},
+        {opr:'resto', func: resto(a, b)}
+    ];
+    for(let [chave, valor] of Object.entries(arr)){
+        result[chave] = valor.func;
+    }
+   var table = gerarTabela(arr);
+   var novo = preecheTabela(table, result);
+   novo.classList = 'tabela';
+   div.appendChild(novo);
 });
